189f54619f07221da23e1691e8dc2a45bb239edf
ZeeShekh1908 <zeenatshekh794@gmail.com>
2024-02-07 09:10:00 +0530

    "Guess a number"

--- Java/RandomNumberGame.java
+++ Java/RandomNumberGame.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class RandomNumberGame {
     public static void main(String[] args) {
@@ -11,18 +12,18 @@ public class RandomNumberGame {
         int guess,i;
         boolean hasWon=false;
         for(i=0;i<t;i++){
-            guess = Integer.parseInt(JOptionPane.showInputDialog(null, "Guess the number:"));
+            guess = Integer.parseInt(JOptionPane.showInputDialog(null,"Guess a Number!!"));
             if(guess==num){
                 JOptionPane.showMessageDialog(null,"Congratulations! you guessed right :)<3");
                 hasWon=true;
                 break;
             }
             else if(guess>num && i!=t-1){
-                JOptionPane.showMessageDialog(null,"Guess is too high");
+                JOptionPane.showMessageDialog(null,"Guess is too high..Guess Again..");
                 score=score-10;
             }
             else if(guess<num && i!=t-1){
-                JOptionPane.showMessageDialog(null,"Guess is too low");
+                JOptionPane.showMessageDialog(null,"Guess is too low..Guess Again..");
                 score=score-10;
             }
         }
@@ -30,7 +31,7 @@ public class RandomNumberGame {
             JOptionPane.showMessageDialog(null,"You won the game in "+t+" tries...");
         }
         if(i==t){
-            System.out.println("You lost all attempts.The number was "+num+" .Play Again!");
+            JOptionPane.showMessageDialog(null,"You lost all attempts :( .The number was "+num+" .Play Again!");
         }
         JOptionPane.showMessageDialog(null, "Game over.");
     }
